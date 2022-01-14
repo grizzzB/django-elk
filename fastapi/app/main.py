378d76a1114148a20e4887affd68b1bef829a94b
grizzzB <kago1230@gmail.com>
2022-01-14 19:19:29 +0900

    템플릿 웹페이지 추가, api 경로 분기 테스트

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -9,6 +9,10 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
+@app.get("/api")
+def read_api():
+    return {"api": "sucess!"}
+
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
