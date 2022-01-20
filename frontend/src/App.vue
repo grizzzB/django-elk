9b1574b9ed5934bad5f5d775fdf0fe5f973a2048
grizzzB <kago1230@gmail.com>
2022-01-21 02:16:14 +0900

    추가 디버그; dco 커맨드 실행 오류(백엔드)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,38 @@
 <template>
   <div id="app">
-    <p>Frontend</p>
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <!-- 추가된 헤더 컴포넌트 사용 -->
+    <Header />
+    
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+	
+    <!-- 추가된 풋터 컴포넌트 사용 -->
+    <Footer />
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
+// import Header from './components/Header.vue'; //import 헤더 추가
+import HelloWorld from './components/HelloWorld.vue'; 
+// import Footer from './components/common/Footer.vue'; //import 풋터 추가
 
 export default {
   name: 'App',
   components: {
-    // HelloWorld
+    // Header          //헤더 컴포넌트 추가
+    HelloWorld
+    // ,Footer         //풋터 컴포넌트 추가
   }
 }
 </script>
 
 <style>
+html,body{padding:0; margin:0;}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin:0; padding:0;
 }
 </style>
---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+	<header>
+		<h1>header</h1>
+		<div class="menuWrap">
+			<ul class="menu">
+				<li><a href="javascript:;">색인 상태 조회</a></li>
+                <li><a href="javascript:;">색인 요청</a></li>
+                <li><a href="javascript:;">색인 중지</a></li>
+                <li><a href="javascript:;">문서 조회</a></li>
+                <li><a href="javascript:;">문서 등록</a></li>
+                <li><a href="javascript:;">문서 수정</a></li>
+				<li><a href="javascript:;">문서 삭제</a></li>
+				<li><a href="javascript:;">문서 벌크 업로드</a></li>
+				<li><a href="javascript:;">문서 벌크 다운로드</a></li>
+                <li><a href="javascript:;">단일 테스트</a></li>
+                <li><a href="javascript:;">벌크 테스트</a></li>
+			</ul>
+		</div>
+	</header>
+</template>
+
+<script>
+export default {
+	
+}
+</script>
+
+<style scoped>
+header{width:100%; text-align:center; position:relative; height:120px; border-bottom:1px solid #35495e}
+header h1{position:absolute; top:0; left:100px;}
+header ul.menu:after{display:block; clear:both; content:'';}
+header ul.menu{position:absolute; top:20px; right:50px;}
+header ul.menu li{float:left; padding:10px 20px; list-style:none;}
+
+a{text-decoration:none; color:#333;}
+</style>
